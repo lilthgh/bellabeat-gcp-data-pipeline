5309a236bade83f5d3adcffbb428dd6ca3feed5a
lilthgh <166909440+lilthgh@users.noreply.github.com>
2026-09-08 13:43:03 +0000

    Save updated view1.sql and json schema files

--- sql/view1.sql
+++ sql/view1.sql
@@ -1,7 +1,9 @@
-Create or replace view `project-a672b30f-4297-4213-ac9.raw_data.view1` As
+CREATE OR REPLACE VIEW `project-a672b30f-4297-4213-ac9.raw_data.view1` As
 select
+Id,
 avg(TotalSteps) as average_steps
 ,avg(TotalDistance) as average_distance,
 avg(Calories) as average_calories
 from
 `project-a672b30f-4297-4213-ac9.raw_data.daily_activity`
+GROUP BY Id;
