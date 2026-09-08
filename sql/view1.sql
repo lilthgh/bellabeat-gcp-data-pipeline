8352fbc12f168133f881e15eea1438b49eba35c3
lilthgh <166909440+lilthgh@users.noreply.github.com>
2026-09-08 23:40:32 +0400

    Update view1.sql
    fixed the group by id

--- sql/view1.sql
+++ sql/view1.sql
@@ -1,7 +1,12 @@
-Create or replace view `project-a672b30f-4297-4213-ac9.raw_data.view1` As
-select
-avg(TotalSteps) as average_steps
-,avg(TotalDistance) as average_distance,
-avg(Calories) as average_calories
-from
-`project-a672b30f-4297-4213-ac9.raw_data.daily_activity`
+CREATE OR REPLACE VIEW `project-a672b30f-4297-4213-ac9.silver_data.view1` AS
+
+SELECT
+  Id,
+  AVG(TotalSteps) AS average_steps,
+  AVG(TotalDistance) AS average_distance,
+  AVG(Calories) AS average_calories
+FROM `project-a672b30f-4297-4213-ac9.raw_data.daily_activity`
+GROUP BY Id;
+
+
+
