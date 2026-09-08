e17b25a5145329c3eece1e96a5447bc3bfdd2c17
lilthgh <166909440+lilthgh@users.noreply.github.com>
2026-09-08 23:45:50 +0400

    Update view2.sql

--- sql/view2.sql
+++ sql/view2.sql
@@ -1,4 +1,4 @@
-create or replace view `project-a672b30f-4297-4213-ac9.raw_data.view2` As
+CREATE OR REPLACE VIEW `project-a672b30f-4297-4213-ac9.raw_data.view2` As
 SELECT
     COUNT(*) AS total_users_num,
     COUNTIF(average_steps BETWEEN 8000 AND 12000) AS goal_completed_users_num
